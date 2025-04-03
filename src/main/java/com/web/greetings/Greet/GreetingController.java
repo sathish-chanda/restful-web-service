476ee3b683559f34616da7f16c09e0a8a793dca7
A-J-007 <alivelu.j1@gmail.com>
2025-04-03 08:08:33 -0400

    Added getting all the greeting whom we already greeted.

--- src/main/java/com/web/greetings/Greet/GreetingController.java
+++ src/main/java/com/web/greetings/Greet/GreetingController.java
@@ -2,6 +2,9 @@ package com.web.greetings.Greet;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,17 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private final List<Greeting> greeted = new ArrayList<>();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		Greeting greet = new Greeting(counter.incrementAndGet(), String.format(template, name));
+        greeted.add(greet);
+		return greet;
+	}
+
+	@GetMapping("/all-greetings") 
+	public List<Greeting> getAllGreetings() {
+		return greeted;
 	}
 }
